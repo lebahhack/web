f4ccd2647c46cb1af67f448128382bbc0021f42a
lebahhack <lebahhack@gmail.com>
2026-05-10 03:44:54 +0700

    Update sitemap.xml.js

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,75 +1,74 @@
 import { getPosts } from "../lib/api";
-import { SITE, sanitizeSlug } from "../lib/config";
+import { SITE } from "../lib/config";
 
 export async function onRequest() {
 
-  const posts = await getPosts();
+const posts=await getPosts();
 
-  // ======================
-  // UNIQUE CATEGORY LIST
-  // ======================
-  const categories = [...new Set(
-    posts.map(p => sanitizeSlug(p.kategori))
-  )];
+const urls=posts.map(post=>`
 
-  const now = new Date().toISOString();
-
-  // ======================
-  // POSTS URL
-  // ======================
-  const postUrls = posts.map(p => {
-    const slug = sanitizeSlug(p.slug);
-    const cat = sanitizeSlug(p.kategori);
-
-    return `
 <url>
-  <loc>${SITE.domain}/${cat}/${slug}</loc>
-  <lastmod>${now}</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.8</priority>
-</url>`;
-  }).join("");
 
-  // ======================
-  // CATEGORY URL
-  // ======================
-  const categoryUrls = categories.map(c => `
+<loc>${SITE.url}/${post.slug}</loc>
+
+<lastmod>${
+new Date(
+post.updated||
+post.created||
+Date.now()
+).toISOString()
+}</lastmod>
+
+<changefreq>daily</changefreq>
+
+<priority>0.8</priority>
+
+</url>
+
 <url>
-  <loc>${SITE.domain}/kategori/${c}</loc>
-  <lastmod>${now}</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>0.7</priority>
-</url>`).join("");
 
-  // ======================
-  // HOME URL
-  // ======================
-  const homeUrl = `
+<loc>${SITE.url}/amp/${post.slug}</loc>
+
+<lastmod>${
+new Date(
+post.updated||
+post.created||
+Date.now()
+).toISOString()
+}</lastmod>
+
+<changefreq>daily</changefreq>
+
+<priority>0.7</priority>
+
+</url>
+
+`).join("");
+
+const xml=`<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset
+xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
 <url>
-  <loc>${SITE.domain}/</loc>
-  <lastmod>${now}</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>1.0</priority>
-</url>`;
 
-  // ======================
-  // FINAL XML
-  // ======================
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<loc>${SITE.url}</loc>
 
-${homeUrl}
+<changefreq>hourly</changefreq>
 
-${categoryUrls}
+<priority>1.0</priority>
 
-${postUrls}
+</url>
+
+${urls}
 
 </urlset>`;
 
-  return new Response(xml, {
-    headers: {
-      "content-type": "application/xml;charset=UTF-8",
-      "cache-control": "public,max-age=3600"
-    }
-  });
+return new Response(xml,{
+headers:{
+"content-type":"application/xml;charset=UTF-8",
+"cache-control":"public,max-age=1800"
+}
+});
+
 }
