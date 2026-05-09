6b968e0ec7b046b0f4a566034e677ec9000b558f
lebahhack <lebahhack@gmail.com>
2026-05-10 03:43:49 +0700

    Update rss.xml.js

--- functions/rss.xml.js
+++ functions/rss.xml.js
@@ -1,71 +1,67 @@
 import { getPosts } from "../lib/api";
-import { SITE, sanitizeSlug } from "../lib/config";
+import { SITE,cleanDescription } from "../lib/config";
 
 export async function onRequest() {
 
-  const posts = await getPosts();
+const posts=(await getPosts())
+.slice(0,30);
 
-  // ======================
-  // SORT NEWEST FIRST
-  // ======================
-  const sorted = posts.reverse();
+const items=posts.map(post=>`
 
-  const items = sorted.slice(0, 50).map(p => {
-
-    const title = escapeXML(p.title || "");
-    const slug = sanitizeSlug(p.slug);
-    const kategori = sanitizeSlug(p.kategori);
-
-    const url = `${SITE.domain}/${kategori}/${slug}`;
-
-    const description = escapeXML(
-      (p.content || "")
-        .replace(/<[^>]*>?/gm, "")
-        .slice(0, 200)
-    );
-
-    const pubDate = new Date().toUTCString();
-
-    return `
 <item>
-  <title>${title}</title>
-  <link>${url}</link>
-  <guid>${url}</guid>
-  <description>${description}</description>
-  <pubDate>${pubDate}</pubDate>
-</item>`;
-  }).join("");
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<title><![CDATA[${post.title}]]></title>
+
+<link>${SITE.url}/${post.slug}</link>
+
+<guid>${SITE.url}/${post.slug}</guid>
+
+<pubDate>${
+new Date(
+post.created||
+Date.now()
+).toUTCString()
+}</pubDate>
+
+<description><![CDATA[${
+cleanDescription(
+post.content||
+"",
+200
+)
+}]]></description>
+
+</item>
+
+`).join("");
+
+const xml=`<?xml version="1.0" encoding="UTF-8"?>
+
 <rss version="2.0">
-<channel>
-  <title>${SITE.name}</title>
-  <link>${SITE.domain}</link>
-  <description>${SITE.description}</description>
-  <language>id-ID</language>
-  <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
 
-  ${items}
+<channel>
+
+<title>${SITE.name}</title>
+
+<link>${SITE.url}</link>
+
+<description>${SITE.description}</description>
+
+<language>id</language>
+
+<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+
+${items}
 
 </channel>
+
 </rss>`;
 
-  return new Response(xml, {
-    headers: {
-      "content-type": "application/xml;charset=UTF-8",
-      "cache-control": "public,max-age=1800"
-    }
-  });
+return new Response(xml,{
+headers:{
+"content-type":"application/xml;charset=UTF-8",
+"cache-control":"public,max-age=1800"
 }
+});
 
-// ======================
-// XML ESCAPE SAFE
-// ======================
-function escapeXML(str = "") {
-  return String(str)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&apos;");
 }
